e85418ff1632c8fe1fa468be98d6a4bdf16c8c92
Yuri Rocha <Yuri Rocha@DESKTOP-R1JRNA0>
2021-09-15 14:26:59 -0300

    Criação do servlet.

---
+++ demo/src/main/java/controller/Login.java
@@ -0,0 +1,44 @@
+package controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Login
+ */
+@WebServlet("/login")
+public class Login extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public Login() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void service(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String usuario = "fulano";
+		String senha = "admin";
+
+		String user = request.getParameter("user");
+		String password = request.getParameter("password");
+
+		if (user.equals(usuario) && password.equals(senha)) {
+			System.out.println("True");
+		} else{
+			System.out.println("False");
+		}
+
+	}
+}
